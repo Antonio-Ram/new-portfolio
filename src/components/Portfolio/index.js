05e3f6de84da179951d35126cc5da2a1e62bc555
Antonio <tonito@umich.edu>
2022-03-06 21:25:35 -0500

    Portfolios working

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import Runbuddy from '../../assets/portfolio/Run-buddy.jpg';
+import Refactor from '../../assets/portfolio/Refactor.jpg';
+import Planner from '../../assets/portfolio/Planner.jpg';
+import ReadMeGen from '../../assets/portfolio/ReadMeGen.jpg';
+import Notes from '../../assets/portfolio/Notes.jpg';
+import Social from '../../assets/portfolio/SocNet.jpg';
 
 function Portfolio() {
     return(
@@ -6,12 +12,29 @@ function Portfolio() {
             <h1>Portfolio</h1>
             <div>
                 <ul>
-                    <li>Challenge 1</li>
-                    <li>Challenge 2</li>
-                    <li>Challenge 3</li>
-                    <li>Challenge 4</li>
-                    <li>Challenge 5</li>
-                    <li>Challenge 6</li>
+                    <li> 
+                        <img src={Runbuddy} style={{ height: "300px" }}/> <a href='https://antonio-ram.github.io/run-buddy/'> Run-Buddy </a>
+                    </li>
+
+                    <li>
+                        <img src={Refactor} style={{ height: "300px" }}/> <a href='https://antonio-ram.github.io/Challenge1/'> 1st code refactor </a>
+                    </li>
+
+                    <li>
+                        <img src={Planner} style={{ height: "300px" }}/> <a href='https://antonio-ram.github.io/DayPlanning/'> Planner </a>
+                    </li>
+
+                    <li>
+                        <img src={ReadMeGen} style={{ height: "300px" }}/> <a href='https://github.com/Antonio-Ram/ReadMePlease'> Read.ME Generator  </a>
+                    </li>
+
+                    <li>
+                        <img src={Notes} style={{ height: "300px" }}/> <a href='https://github.com/Antonio-Ram/NoteTaker'> Note Taker </a>
+                    </li>
+
+                    <li>
+                        <img src={Social} style={{ height: "300px" }}/> <a href='https://antonio-ram.github.io/NoSQLapi/'> Social Network </a>
+                    </li>
                 </ul>
             </div>
         </section>
